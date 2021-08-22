20087a2d2e69f0c2133f4b91b09f586eac092361
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-08-23 00:16:03 +0800

    開始學習dart

---
+++ Dart/dart1-1.dart
@@ -0,0 +1,22 @@
+void main() {
+  
+  var a = 1;  //int
+  var b = "1"; //string
+  var c = 1.1;  //double
+  
+  if(a is int){
+    print("a is int");
+    
+  }
+  
+  if(b is String){
+    print("b is String");
+  }
+  
+  if(c is double){
+    print("c is double");
+  }
+  
+  
+  
+}
